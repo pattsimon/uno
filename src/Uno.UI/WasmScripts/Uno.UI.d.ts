29d31f353db88cc55f232cba2d087f117a42902c
Martin Zikmund <martin@zikmund.dev>
2022-02-08 16:29:20 +0100

    perf: Native marshalled border style setter methods

--- src/Uno.UI/WasmScripts/Uno.UI.d.ts
+++ src/Uno.UI/WasmScripts/Uno.UI.d.ts
@@ -305,6 +305,16 @@ declare namespace Uno.UI {
         *
         */
         setStyleDoubleNative(pParams: number): boolean;
+        setSolidColorBorder(htmlId: number, colorHex: string, width: string): boolean;
+        /**
+        * Set border to solid color brush.
+        */
+        setSolidColorBorderNative(pParams: number): boolean;
+        setGradientBorder(htmlId: number, borderImage: string, width: string): boolean;
+        /**
+        * Set border to gradient brush.
+        */
+        setGradientBorderNative(pParams: number): boolean;
         setArrangeProperties(elementId: number): string;
         /**
             * Remove the CSS style of a html element.
@@ -1454,6 +1464,12 @@ declare class WindowManagerSetElementTransformParams {
     M32: number;
     static unmarshal(pData: number): WindowManagerSetElementTransformParams;
 }
+declare class WindowManagerSetGradientBorderParams {
+    HtmlId: number;
+    BorderImage: string;
+    Width: string;
+    static unmarshal(pData: number): WindowManagerSetGradientBorderParams;
+}
 declare class WindowManagerSetNameParams {
     HtmlId: number;
     Name: string;
@@ -1469,6 +1485,12 @@ declare class WindowManagerSetPropertyParams {
     Pairs_Length: number;
     Pairs: Array<string>;
     static unmarshal(pData: number): WindowManagerSetPropertyParams;
+}
+declare class WindowManagerSetSolidColorBorderParams {
+    HtmlId: number;
+    ColorHex: string;
+    Width: string;
+    static unmarshal(pData: number): WindowManagerSetSolidColorBorderParams;
 }
 declare class WindowManagerSetStyleDoubleParams {
     HtmlId: number;
